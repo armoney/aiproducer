7e1c2727db245500205e0bab79e4614781c67f97
Armon Arcuri <armonarcuri@Armons-MBP.lan1>
2023-08-14 12:04:56 -0700

    Add Prettier

--- app/home/page.js
+++ app/home/page.js
@@ -4,7 +4,9 @@ export default function Page() {
   return (
     <>
       <h1>Welcome to AI Producer</h1>
-      <Link href="/producer" prefetch={false}>Start video</Link>
+      <Link href="/producer" prefetch={false}>
+        Start video
+      </Link>
     </>
-  )
+  );
 }
--- app/page.js
+++ app/page.js
@@ -1,14 +1,16 @@
-import Image from 'next/image'
-import styles from './page.module.css'
-import Link from 'next/link';
+import Image from "next/image";
+import styles from "./page.module.css";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <>
       <h1>AI PRODUCER</h1>
-      <Link href="/home" prefetch={true}>Home</Link>
+      <Link href="/home" prefetch={true}>
+        Home
+      </Link>
     </>
-  )
+  );
   // return (
   //   <main className={styles.main}>
   //     <div className={styles.description}>
