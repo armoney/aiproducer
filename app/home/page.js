60e864a829118dc510a736958e225433c8c46b15
Armon Arcuri <armonarcuri@Armons-MacBook-Pro.local>
2023-08-16 11:33:11 -0700

    Add tailwind, add Gradient, test vimeo video

--- app/home/page.js
+++ app/home/page.js
@@ -1,11 +1,29 @@
+"use client";
+import { useEffect } from "react";
 import Calendly from "../components/calendly";
 import Link from "next/link";
+import { gradient } from "../utils/Gradient";
+import "./page.css";
 
 export default function Page() {
+  useEffect(() => {
+    gradient.initGradient("#gradient-canvas");
+  });
+
   return (
     <>
-      <h1>Welcome to AI Producer</h1>
-
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+      <canvas id="gradient-canvas" data-transition-in />
+      <iframe
+        title="vimeo-player"
+        src="https://player.vimeo.com/video/828795162?h=0fc5665123"
+        width="640"
+        height="360"
+        frameborder="0"
+        allowfullscreen
+      ></iframe>
       <Calendly />
 
       <Link href="/producer" prefetch={false}>
--- app/page.js
+++ app/page.js
@@ -1,11 +1,10 @@
-import Image from "next/image";
-import styles from "./page.module.css";
 import Link from "next/link";
 
 export default function Home() {
   return (
     <>
       <h1>AI PRODUCER</h1>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
       <Link href="/home" prefetch={true}>
         Home
       </Link>
