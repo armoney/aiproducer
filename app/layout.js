fc59bdc25f6fad583802a9fd8fab708df0840456
Armon Arcuri <armonarcuri@Armons-MBP.lan1>
2023-08-17 16:06:38 -0700

    Add script to parent

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import "./globals.css";
 import { Inter } from "next/font/google";
+import Script from 'next/script'
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -11,6 +12,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <Script src="https://js.stripe.com/v3/" />
       <body className={inter.className}>{children}</body>
     </html>
   );
