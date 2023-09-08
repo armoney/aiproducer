50a787fc06ff61a9446e4a16c3e6b7c4ab6472b0
Armon Arcuri <armonarcuri@Armons-MacBook-Pro.local>
2023-09-08 13:54:42 -0700

    Fix form styles

--- app/components/form/textInput.js
+++ app/components/form/textInput.js
@@ -5,7 +5,7 @@ const TextInput = React.forwardRef(({ onChange, id, name, label }, ref) => (
     <span className="text-gray-700">{label}</span>
     <input
       type="text"
-      className="form-input mt-1 block w-full"
+      className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 hover:border-gray-400"
       placeholder=""
       id={id}
       name={name}
