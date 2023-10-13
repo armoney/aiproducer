71bf3370099d7bc530a73ab2ff0448f04c7e02da
Armon Arcuri <armonarcuri@armonarcuri.local>
2023-10-13 16:08:48 -0700

    Add navbar around

--- app/questionnaire/page.js
+++ app/questionnaire/page.js
@@ -1,5 +1,13 @@
 import Questionnaire from "../components/questionnaire";
+import { Navigation } from "../components/navigation";
 
 export default function Page() {
-  return <Questionnaire />;
+  return (
+    <>
+      <Navigation />
+      <div className="pt-20">
+        <Questionnaire />
+      </div>
+    </>
+  );
 }
