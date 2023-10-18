8258e9687571bf8eb48ebebf80625a24c3c6ae1c
Armon Arcuri <armonsf@gmail.com>
2023-10-18 16:00:33 -0700

    Update image (#29)
    Co-authored-by: Armon Arcuri <armonarcuri@armonarcuri.local>

--- app/about/page.js
+++ app/about/page.js
@@ -1,6 +1,6 @@
 import { Navigation } from "../components/navigation";
 import Image from "next/image";
-import JoshImg from "../../public/josh.jpeg";
+import JoshImg from "../../public/Josh_small.png";
 import ArmonImg from "../../public/armon.jpeg";
 const About = () => {
   const teamMembers = [
@@ -26,7 +26,7 @@ const About = () => {
     return (
       <li key={member.id}>
         <Image
-          className="rounded-2xl	object-cover	w-full aspect-[3/2] h-auto"
+          className="rounded-2xl	object-cover	w-full aspect-[2/3] h-auto"
           src={member.img}
           alt={member.name}
         />
@@ -42,9 +42,9 @@ const About = () => {
             viewBox="0 0 20 20"
           >
             <path
-              fill-rule="evenodd"
+              fillRule="evenodd"
               d="M16.338 16.338H13.67V12.16c0-.995-.017-2.277-1.387-2.277-1.39 0-1.601 1.086-1.601 2.207v4.248H8.014v-8.59h2.559v1.174h.037c.356-.675 1.227-1.387 2.526-1.387 2.703 0 3.203 1.778 3.203 4.092v4.711zM5.005 6.575a1.548 1.548 0 11-.003-3.096 1.548 1.548 0 01.003 3.096zm-1.337 9.763H6.34v-8.59H3.667v8.59zM17.668 1H2.328C1.595 1 1 1.581 1 2.298v15.403C1 18.418 1.595 19 2.328 19h15.34c.734 0 1.332-.582 1.332-1.299V2.298C19 1.581 18.402 1 17.668 1z"
-              clip-rule="evenodd"
+              clipRule="evenodd"
             ></path>
           </svg>
         </a>
@@ -55,7 +55,7 @@ const About = () => {
     <div>
       <Navigation />
       <div className="max-w-2xl m-auto sm:max-w-xl md:max-w-2xl lg:max-w-5xl xl:max-w-7xl pt-48 px-8">
-        <ul className="pb-20 grid gap-x-8 gap-y-20 grid-cols-1 md:grid-cols-2 xl:grid-cols-3">
+        <ul className="pb-20 grid gap-x-28 gap-y-20 grid-cols-1 md:grid-cols-2 xl:grid-cols-3">
           <li className="max-w-2xl	md:col-span-2 lg:col-span-3">
             <h2 className="text-gray-900 text-3xl	font-bold	tracking-tight md:text-4xl">
               Our team
