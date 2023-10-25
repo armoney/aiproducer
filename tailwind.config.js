07f13dede8bfc7b8df03a59d6ef28736333f50a7
Armon Arcuri <armonarcuri@armonarcuri.local>
2023-10-25 01:39:48 -0700

    Fix safari min-width bug

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,9 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    minWidth: {
+      '20': '20px',
+    },
     extend: {
       height: {
         144: "36rem",
