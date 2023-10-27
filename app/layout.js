278d17d2e4fce4a9c58d7bf17396dcc9a382df15
Armon Arcuri <armonarcuri@armonarcuri.local>
2023-10-26 20:29:05 -0700

    Add analytics

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,5 @@
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/react";
 import { Inter } from "next/font/google";
 import Script from "next/script";
 import { StrictMode } from "react";
@@ -14,7 +15,10 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <Script src="https://js.stripe.com/v3/" />
       <StrictMode>
-        <body className={inter.className}>{children}</body>
+        <body className={inter.className}>
+          {children}
+          <Analytics />
+        </body>
       </StrictMode>
     </html>
   );
