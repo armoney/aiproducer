1551081ee43e337d9639228dc4cceb63dafe3f48
Armon Arcuri <armonarcuri@armonarcuri.local>
2023-11-02 13:41:29 -0700

    Will this fix it?

--- app/page.js
+++ app/page.js
@@ -62,7 +62,7 @@ export default function Page() {
             </p>
             <div className="mt-10 flex items-center justify-center gap-x-6 xl:justify-start">
               <a
-                href="/producer/questionnaire"
+                href="/#pricing"
                 className="rounded-md bg-primary-500 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-primary-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary-500"
               >
                 Get started
