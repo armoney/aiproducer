11b662ce7ee5058d426c7d089473ad0c842065a3
Armon Arcuri <armonarcuri@armonarcuri.local>
2023-12-01 12:11:06 -0800

    Update Next

---
+++ app/(marketing)/blog/page.js
@@ -0,0 +1,16 @@
+import { getSortedPostsData } from "../../../lib/posts";
+async function getPosts() {
+  const allPostsData = getSortedPostsData();
+  // console.log("allPostsData: ", allPostsData);
+  return {
+    props: {
+      allPostsData,
+    },
+  };
+}
+
+export default async function Index() {
+  const posts = await getPosts();
+  // console.log("posts: ", posts.props.allPostsData);
+  return posts.props.allPostsData.map((post) => <div>{post.title}</div>);
+}
