6de1c2f7131cd54409ffc28252390e1860ecaf52
Armon Arcuri <armonsf@gmail.com>
2024-01-29 20:50:01 -0800

    Update stripe checkout (#70)
    Co-authored-by: Armon Arcuri <armonarcuri@armonarcuri.lan>

--- app/components/payment.js
+++ app/components/payment.js
@@ -1,57 +1,42 @@
 "use client";
-import Script from "next/script";
-import { useEffect, memo } from "react";
-import { useRouter, useSearchParams } from "next/navigation";
+import { useEffect, useState, memo } from "react";
+import { useSearchParams } from "next/navigation";
+import { loadStripe } from "@stripe/stripe-js";
+import {
+  EmbeddedCheckoutProvider,
+  EmbeddedCheckout,
+} from "@stripe/react-stripe-js";
+
+const stripePromise = loadStripe(
+  process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY
+);
 
 const Payment = memo(function Payment() {
-  const router = useRouter();
+  const [clientSecret, setClientSecret] = useState("");
   const searchParams = useSearchParams();
   const jpid = searchParams.get("jpid");
   const name = searchParams.get("name");
   const price = searchParams.get("price");
 
   useEffect(() => {
-    const stripe = Stripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY);
-
-    initialize();
-
-    // Fetches a session and captures the client secret
-    async function initialize() {
-      const response = await fetch(
-        `/api/stripe?jpid=${jpid}&name=${name}&price=${price}`,
-        {
-          method: "POST",
-        }
-      );
-
-      const { clientSecret } = await response.json();
-
-      // Example `onComplete` callback
-      // const handleComplete = function () {
-      //   // Unmount Checkout
-      //   checkout.unmount();
-
-      //   // Retrieve details from server (which loads Checkout Session)
-      //   // const details = await retrievePurchaseDetails();
-
-      //   // Show custom purchase summary
-      //   router.push("/producer/confirmation");
-      //   // showPurchaseSummary(details);
-      // };
-      const checkout = await stripe.initEmbeddedCheckout({
-        clientSecret,
-      });
-
-      // Mount Checkout
-      checkout.mount("#checkout");
-    }
+    fetch(`/api/stripe?jpid=${jpid}&name=${name}&price=${price}`, {
+      method: "POST",
+    })
+      .then((res) => res.json())
+      .then((data) => setClientSecret(data.clientSecret));
   }, [jpid]);
+
+  const options = { clientSecret };
+  console.log(clientSecret);
 
   return (
     <>
-      <Script src="https://js.stripe.com/v3/" strategy="beforeInteractive" />
       <div id="checkout">
-        {/* <!-- Checkout will insert the payment form here --> */}
+        {clientSecret && (
+          <EmbeddedCheckoutProvider stripe={stripePromise} options={options}>
+            <EmbeddedCheckout />
+          </EmbeddedCheckoutProvider>
+        )}
       </div>
     </>
   );
