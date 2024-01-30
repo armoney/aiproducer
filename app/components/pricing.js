4457cdc65a45788a521f364a709cde665a3cf737
Armon Arcuri <armonarcuri@armonarcuri.lan>
2024-01-29 20:48:32 -0800

    Update stripe checkout

--- app/components/pricing.js
+++ app/components/pricing.js
@@ -142,7 +142,7 @@ export default function Pricing() {
               <div className="mt-10 flex items-center justify-center gap-x-6 xl:justify-start">
                 <Link
                   prefetch={true}
-                  href="/producer/checkout?name=Combination&price=39900"
+                  href="/producer/checkout?name=Combination&price=29900"
                   className="rounded-md bg-primary-500 w-full px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-primary-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary-500"
                 >
                   Get started today
